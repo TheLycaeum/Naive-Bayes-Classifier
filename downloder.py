41e9dbefd3f36a331efbd149566e83e164b5e247
fairoos <fairoosnm123@gmail.com>
2019-02-25 21:25:52 +0530

    Added function for downlode a file from web using it's url. make a smart progress bar with the help of tqdm for visualisation.

--- downloder.py
+++ downloder.py
@@ -1,5 +1,19 @@
 from tqdm import tqdm
-import time
+"""tqdm is a smart progress meter""" 
+import requests
 
-for x in tqdm(range(1000)):
-    time.sleep(0.01)
+file_name = input("Enter your song's name: ")
+url = input("Enter your URL here: ")
+def downlod_file(url, file_name):
+    chunk_size = 1024
+    r = requests.get(url, stream = True)
+    total_size = int(r.headers['content-length'])
+    
+    with open(file_name, 'wb') as f:
+        for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'KB'):
+            f.write(data)
+
+    print("Download completed!")
+
+    
+downlod_file(url, file_name)
