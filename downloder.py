1957861676773a0c25571de9d0d99390cf59e3a8
fairoos <fairoosnm123@gmail.com>
2019-02-25 23:04:41 +0530

    Refactor: Changed function name downlod_file to downlod_lyrics

--- downloder.py
+++ downloder.py
@@ -2,12 +2,11 @@ import requests
 import bs4 #for pulling data out of HTML and XML files
 
 url = input("Enter your URL here: ")
-def downlod_file(url):
+def downlod_lyrics(url):
+    """downlod lyrics form url and return as string format"""
     res = requests.get(url)
     data = bs4.BeautifulSoup(res.text, 'lxml')
     lyrics = " "
     for i in data.select('.verse'):
         lyrics += i.text
     return  lyrics
-    
-print(downlod_file(url))
