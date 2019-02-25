352adf5da5a226eace5d0bdae825a365bcd184ee
fairoos <fairoosnm123@gmail.com>
2019-02-25 21:39:13 +0530

    Imported bs4 for pulling data out of HTML and XML files.

--- downloder.py
+++ downloder.py
@@ -1,14 +1,14 @@
-from tqdm import tqdm
-"""tqdm is a smart progress meter""" 
+from tqdm import tqdm # tqdm is a smart progress meter
 import requests
+import bs4
 
 file_name = input("Enter your song's name: ")
 url = input("Enter your URL here: ")
 def downlod_file(url, file_name):
     chunk_size = 1024
     r = requests.get(url, stream = True)
-    total_size = int(r.headers['content-length'])
     
+    total_size = int(r.headers['content-length'])  
     with open(file_name, 'wb') as f:
         for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'KB'):
             f.write(data)
