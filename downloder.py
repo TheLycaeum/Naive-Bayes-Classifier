cb695a3d9890061c80820ba86bccbba769ed18de
fairoos <fairoosnm123@gmail.com>
2019-02-25 22:52:16 +0530

    Removed tqdm and everything related to it It not nessory in here Rename lyrics to data in this line lyrics = bs4.BeautifulSoup(res.text, 'lxml') Added variable string "lyrics" to store i from for loop

--- downloder.py
+++ downloder.py
@@ -1,20 +1,13 @@
-from tqdm import tqdm # tqdm is a smart progress meter
 import requests
 import bs4 #for pulling data out of HTML and XML files
 
-file_name = input("Enter your song's name: ")
 url = input("Enter your URL here: ")
-def downlod_file(url, file_name):
-    chunk_size = 1024
+def downlod_file(url):
     res = requests.get(url)
-    lyrics = bs4.BeautifulSoup(res.text, 'lxml')
-    for i in lyrics.select('.verse'):
-        total_size = int(res.headers['content-length'])  
-        with open(file_name, 'wb') as f:
-            for data in tqdm(iterable = res.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'KB'):
-                f.write(data)
-
-            print("Download completed!")
-        return  print(i.text)
+    data = bs4.BeautifulSoup(res.text, 'lxml')
+    lyrics = " "
+    for i in data.select('.verse'):
+        lyrics += i.text
+    return  lyrics
     
-downlod_file(url, file_name)
+print(downlod_file(url))
