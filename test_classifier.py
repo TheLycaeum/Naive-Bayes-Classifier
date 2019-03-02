cd1070c5b8e221daae31360ddb717045ed690d88
fairoos <fairoosnm123@gmail.com>
2019-03-02 11:56:58 +0530

    Modified: modified path of traning data.

--- test_classifier.py
+++ test_classifier.py
@@ -1,7 +1,9 @@
 import classifier
 
 def test_words_in_a_folder():
-    path = '/home/fairoos/naive_byaes/sports'
+    path = '/home/fairoos/naive_byaes/training_data/sports'
     assert classifier.words_in_a_folder(path) == ['A', 'A', 'Very', 'but', 'clean', 'clean', 'forgettable', 'game', 'game', 'great', 'match']
-    path1 = '/home/fairoos/naive_byaes/non_sports'
+    path1 = '/home/fairoos/naive_byaes/training_data/non_sports'
     assert classifier.words_in_a_folder(path1) == ['It', 'The', 'The', 'a', 'barking', 'beautful', 'close', 'dogs', 'election', 'election', 'flower', 'over', 'was', 'was']
+
+    
