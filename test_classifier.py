4bfc7939bc579a5d9807362ea8d70b82cf6a260b
fairoos <fairoosnm123@gmail.com>
2019-03-02 12:12:29 +0530

    RED: Added test for possible_words

--- test_classifier.py
+++ test_classifier.py
@@ -6,4 +6,5 @@ def test_words_in_a_folder():
     path1 = '/home/fairoos/naive_byaes/training_data/non_sports'
     assert classifier.words_in_a_folder(path1) == ['It', 'The', 'The', 'a', 'barking', 'beautful', 'close', 'dogs', 'election', 'election', 'flower', 'over', 'was', 'was']
 
-    
+def test_possible_words:
+    assert classifier.possible_words(['hello', 'hi', 'good']) == 3 
