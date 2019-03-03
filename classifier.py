368e154b6c5861b5c9b59a3fcf8fc69741714e32
fairoos <fairoosnm123@gmail.com>
2019-03-03 11:49:54 +0530

    Added function for calculating probability.

--- classifier.py
+++ classifier.py
@@ -27,5 +27,45 @@ def possible_words():
     count = len(list_words)
     return count
 
-
+# def words_in_a_file(path):
+#     for filename in os.listdir(path):
+#         print(filename)
+#         file_path = os.path.join(path, filename)
+#         fil = open(file_path)
+#         content = fil.read()
+#     return content
     
+    
+def probability():
+    path = '/home/fairoos/naive_byaes/testing_data'
+    for filename in os.listdir(path):
+        print(filename)
+        file_path = os.path.join(path, filename)
+        fil = open(file_path)
+        content = fil.read()
+        files_cont = re.sub(r'[^\w\s]','',content) #remove punctuation
+        cont_list = files_cont.split()
+        cont_list.sort()
+        cont_list = [x.lower() for x in cont_list] #to lower case all words
+        Dir_of_data = '/home/fairoos/naive_byaes/training_data/'
+        for dir in os.listdir(Dir_of_data):
+            print(dir)
+            words =  words_in_a_folder(f'/home/fairoos/naive_byaes/training_data/{dir}')
+            b =[]
+            for element in cont_list:
+                count = 0
+                for word in words:
+                    if element == word:
+                        count += 1
+                b.append(count)
+            c = []
+            for count in b:
+                p_a_word = ((count + 1)/(len(words) + possible_words()))
+                c.append(p_a_word)
+            probability = 1
+            for d in c:
+                probability = probability * d
+            pro = format(float(probability), 'f')
+            print(pro)
+                
+probability()
