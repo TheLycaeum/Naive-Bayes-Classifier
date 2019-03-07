2d89f1a0a6f5d6d9d7b54c7c34012a35dc2807ac
fairoos <fairoosnm123@gmail.com>
2019-03-07 21:39:26 +0530

    GREEN: test for possible words passed. GREEN:Changed path in words_in_a_folder.

--- test_classifier.py
+++ test_classifier.py
@@ -2,13 +2,13 @@ import classifier
 import pytest
 
 def test_words_in_a_folder():
-    path = '/home/fairoos/naive_byaes/pytest_data/training_data/sports lyrics'
+    path = '/home/fairoos/naive_byaes/pytest_data/training_data/sports data'
     assert classifier.words_in_a_folder(path) == ['clean', 'clean', 'forgettable', 'game', 'game', 'great', 'match']
-    path1 = '/home/fairoos/naive_byaes/pytest_data/training_data/non_sports lyrics'
+    path1 = '/home/fairoos/naive_byaes/pytest_data/training_data/non_sports data'
     assert classifier.words_in_a_folder(path1) == ['close', 'election', 'election']
 
-def test_possible_words:
-    assert classifier.possible_words(['hello', 'hi', 'good']) == 3 
+def test_possible_words():
+    assert classifier.possible_words(Dir_of_data = '/home/fairoos/naive_byaes/pytest_data/training_data/') == 7 
 
 # def test_probability_word():
 #     word = 'a'
