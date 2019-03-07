19446a498fdda707cb020f6b66d41d35ded72c13
fairoos <fairoosnm123@gmail.com>
2019-03-08 03:13:59 +0530

    RED:Added test for probability

--- test_classifier.py
+++ test_classifier.py
@@ -1,5 +1,9 @@
 import classifier
-import pytest
+#import pytest
+import status_test
+
+train_data = '/home/fairoos/naive_byaes/pytest_data/training_data/'
+test_data = '/home/fairoos/naive_byaes/pytest_data/testing_data'
 
 def test_words_in_a_folder():
     path = '/home/fairoos/naive_byaes/pytest_data/training_data/sports data'
@@ -8,14 +12,19 @@ def test_words_in_a_folder():
     assert classifier.words_in_a_folder(path1) == ['close', 'election', 'election']
 
 def test_possible_words():
-    assert classifier.possible_words(Dir_of_data = '/home/fairoos/naive_byaes/pytest_data/training_data/') == 7
+    assert classifier.possible_words(train_data) == 7
 
 def test_stop_word():
     assert classifier.stop_word(['the', 'monkey', 'is', 'not', 'a', 'cat']) == ['monkey', 'cat']
 
 def test_probability_dict():
-    assert classifier.probability_dict(['game', 'match'], Dir_of_data = '/home/fairoos/naive_byaes/pytest_data/training_data/') == {'sports data': 9.111761218400412e-07, 'non_sports data': 3.038169126368391e-07}
+    assert classifier.probability_dict(['game', 'match'], train_data) == {'sports data': 0.0008640552995391705, 'non_sports data': 0.0004032258064516129}
 
 def test_percent_calculator():
     assert classifier.percent_calculator({'sports data': 9.111761218400412e-07, 'non_sports data': 3.038169126368391e-07}) == {'sports data': 74.99434943117606, 'non_sports data': 25.005650568823928}
 
+def test_probability():
+    assert classifier.probability(test_data, train_data) == [{'non_sports data': 48.27586206896553, 'sports data': 51.72413793103448}, {'non_sports data': 31.818181818181824, 'sports data': 68.18181818181816}]
+
+# def test_status_test():
+#     assert status_test.name_file(file_names = os.listdir(test_data)) == ['non_sports data', 'sports data']
